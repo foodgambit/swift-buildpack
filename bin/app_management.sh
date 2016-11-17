f627c003da6221a21c01101ba01b25c6919fa264
rolivieri <roliv@us.ibm.com>
2016-11-17 16:40:38 -0600

    Adding app-management files (work in progress).

---
+++ bin/app_management.sh
@@ -0,0 +1,213 @@
+#!/usr/bin/env bash
+# IBM SDK for Node.js Buildpack
+# Copyright 2015 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+function installAgent() {
+  mkdir $BUILD_DIR/.app-management
+  mkdir $BUILD_DIR/.app-management/scripts
+  mkdir $BUILD_DIR/.app-management/utils
+  mkdir $BUILD_DIR/.app-management/bin
+  mkdir $BUILD_DIR/.app-management/handlers
+  mkdir $BUILD_DIR/.app-management/mosquitto
+
+  cp -r $BP_DIR/app_management/bin/* $BUILD_DIR/.app-management/bin
+  cp $BP_DIR/app_management/bin/agent_api_version $BUILD_DIR/..
+  cp $BP_DIR/app_management/scripts/* $BUILD_DIR/.app-management/scripts
+  cp $BP_DIR/app_management/utils/* $BUILD_DIR/.app-management/utils
+  cp -ra $BP_DIR/app_management/handlers/* $BUILD_DIR/.app-management/handlers
+  # Note: $BUILD_DIR/.app-management/handlers/ will be pruned by setupHandlerBinaries() below
+  cp $BP_DIR/app_management/initial_startup.rb $BUILD_DIR/vendor
+  cp -r $BP_DIR/app_management/mosquitto/* $BUILD_DIR/.app-management/mosquitto
+  cp $BP_DIR/app_management/env.json $BUILD_DIR/.app-management
+
+  chmod +x $BUILD_DIR/.app-management/bin/proxyAgent
+  chmod +x $BUILD_DIR/.app-management/scripts/*
+  chmod +x $BUILD_DIR/.app-management/utils/*
+  chmod -R +x $BUILD_DIR/.app-management/handlers/
+  chmod +x $BUILD_DIR/vendor/initial_startup.rb
+}
+
+function getOpts(){
+  local start=$1
+  # match all before and including the start script
+  local dirtyOpts=$(echo $start | grep -oP "node\s[^-]\S*|.*node\s.*?\s[^-]\S*" | command head -n 1)
+  # match anything starting with at least one '-' followed by any non-whitespace, then at least 1 space
+  local opts=$(echo $dirtyOpts | grep -oP "(^[-]\S*|\s[-]+\S*)")
+  echo $opts
+}
+
+function getArgs(){
+  local start=$1
+  local dirtyOpts=$(echo $start | grep -oP "node\s[^-]\S*|.*node\s.*?\s[^-]\S*" | command head -n 1)
+  # get the second half (the args) by removing the dirty opts.
+  local args="${start/$dirtyOpts/}"
+  echo $args
+}
+
+function updateStartCommands() {
+  # Update start command on start script (used by agent/initial startup)
+  if test -f ${BUILD_DIR}/Procfile; then
+    local start_command=$(sed -n -e '/^web:/p' ${BUILD_DIR}/Procfile | sed 's/^web: //')
+    sed -i s#%COMMAND%#"${start_command}"# "${BUILD_DIR}"/.app-management/scripts/start
+  
+    # Use initial_startup to start application
+    sed -i 's#web:.*#web: ./vendor/initial_startup.rb#' $BUILD_DIR/Procfile
+  else
+    sed -i s#%COMMAND%#"npm start"# "${BUILD_DIR}"/.app-management/scripts/start
+    # Use initial_startup to start application
+    touch $BUILD_DIR/Procfile
+    echo "web: ./vendor/initial_startup.rb" > $BUILD_DIR/Procfile
+  fi
+
+  # Logic to preserve the opts and args for the start command.
+  if [[ $start_command == *"node"* ]]; then
+    opts=$(getOpts "$start_command")
+    args=$(getArgs "$start_command")
+  elif [ -f "${BUILD_DIR}/package.json" ]; then
+    start_script=$(cat "$BUILD_DIR/package.json" | "$BP_DIR/vendor/jq" -r .scripts.start)
+    if [[ -n $start_script ]] && [[ ! "$start_script" == "" ]]; then
+      opts=$(getOpts "$start_script")
+      args=$(getArgs "$start_script")
+    fi
+  fi
+
+  # Update env vars used for dev mode
+  echo "export BOOT_SCRIPT=${boot_js_file}" >> ${BUILD_DIR}/.profile.d/bluemix_env.sh
+  echo "export NODE_EXECUTABLE=node" >> ${BUILD_DIR}/.profile.d/bluemix_env.sh
+  echo "export NODE_OPTS=\"${opts}\"" >> ${BUILD_DIR}/.profile.d/bluemix_env.sh
+  echo "export NODE_ARGS=\"${args}\"" >> ${BUILD_DIR}/.profile.d/bluemix_env.sh
+}
+
+# Sets up a Node runtime at `$BUILD_DIR/.app-management/node`. It will either be a simple
+# symlink to `/home/vcap/app/vendor/node` or a copy of app_management's Node runtime.
+function copyAppManagementNode() {
+  local appmgmt_node_version=$(${BP_DIR}/app_management/node/bin/node -v)
+  local runtime_node_version=$(${BUILD_DIR}/vendor/node/bin/node -v)
+  [[ "$enabled_handlers" ]] || enabled_handlers=$($BP_DIR/lib/print_enabled_handlers.rb)
+
+  # Determine if we must copy the whole Node runtime from the app_management folder, or
+  # whether a symlink will suffice. The strategy is: if any handlers are enabled in the env,
+  # and the Node version under which they were built differs from vendor/node, then we must
+  # copy the runtime to ensure compatibility.
+  local build_app_mgmt_dir=${BUILD_DIR}/.app-management
+  if [[ "$enabled_handlers" != "" && "$appmgmt_node_version" != "$runtime_node_version" ]]; then
+    cp -ra ${BP_DIR}/app_management/node ${build_app_mgmt_dir}
+    info "  Installing common dependency: App Management Node runtime"
+  else
+    # Otherwise, we can get away with a symlink node -> ../vendor/node
+    ( cd ${build_app_mgmt_dir} && ln -sf ../vendor/node node )
+  fi
+
+  # Fix up the npm symlink if it has lost its magic
+  if [[ ! -L ${build_app_mgmt_dir}/node/bin/npm ]]; then
+    ( cd ${build_app_mgmt_dir}/node/bin && ln -fs ../lib/node_modules/npm/bin/npm-cli.js npm )
+  fi
+}
+
+# $1: list of handlers, separated by whitespace
+# $2: a handler name
+# Prints $2 if $2 appears in the list. Otherwise prints ""
+function isHandlerEnabled() {
+  for i in $1; do
+    if [[ "$i" == "$2" ]]; then
+      echo "$2"
+      return
+    fi
+  done
+  echo ""
+}
+
+# Prune the handlers in $BUILD_DIR: delete the node_modules folder of any handler
+# that is not enabled in the environment. Copy the app_management Node runtime
+# if necessary.
+function setupHandlerBinaries() {
+  [[ "$enabled_handlers" ]] || enabled_handlers=$($BP_DIR/lib/print_enabled_handlers.rb)
+  local handlers_list=$(echo $enabled_handlers | sed 's/\s/, /g')
+  if [ "$handlers_list" != "" ]; then
+    info "The following utilities are configured to be enabled and will be installed now: $handlers_list"
+  fi
+
+  for dir in $BUILD_DIR/.app-management/handlers/start-*; do
+    base=$(basename $dir) # start-foo
+    name=${base#start-}   # foo
+    if [[ $(isHandlerEnabled "$enabled_handlers" "$name") == "" ]]; then
+      # foo is not enabled; delete its binaries
+      if [[ -d $dir/node_modules ]]; then
+        rm -rf $dir/node_modules
+      fi
+    else
+      # foo is enabled; indicate to user that it got installed
+      info "  Installing utility: $name"
+    fi
+  done
+
+  # Copy the app management's bundled Node runtime if necessary
+  copyAppManagementNode
+}
+
+function generateAppMgmtInfo() {
+  local CONTAINER="warden"
+  local SSH_ENABLED="false"
+  local PROXY_SUPPORTED='["v1", "v2"]'
+
+  # Generate app management info file. proxy_enabled is set during startup.
+cat > $BUILD_DIR/.app-management/app_mgmt_info.json << EOL
+{
+  "container": "$CONTAINER",
+  "ssh_enabled": $SSH_ENABLED,
+  "proxy_enabled": false,
+  "proxy_supported_version": $PROXY_SUPPORTED
+}
+EOL
+
+}
+
+function installBMCmodule() {
+  npm_install $BP_DIR/resources/rmu/bluemix-management-client
+  # create enhanced copy of boot script
+  createEnhancedStart ${BUILD_DIR}/vendor 'require("bluemix-management-client");'
+}
+
+function createEnhancedStart() {
+  folder=$1
+  header=$2
+  boot_enhanced="$(basename $boot_js_file).enhanced"
+  touch ${folder}/${boot_enhanced}
+  cp $BUILD_DIR/$boot_js_file ${folder}/${boot_enhanced}
+  add_header ${folder}/${boot_enhanced} "${header}"
+}
+
+function installAppManagement() {
+  # Find boot script file
+  boot_js_file=$($BP_DIR/bin/find_boot_script $BUILD_DIR)
+
+  if [ "$boot_js_file" == "" ]; then
+    info "WARN: App Management cannot be installed because the start script cannot be found."
+    info "To install App Management utilities, specify your 'node' start script in 'package.json' or 'Procfile'."
+  else
+    if test -f $BUILD_DIR/package.json; then
+    dependencies=$(cat $BUILD_DIR/package.json | $BP_DIR/vendor/jq -r .dependencies)
+      if [[ $dependencies == *"bunyan"* ]] || [[ $dependencies == *"log4js"* ]] || [[ $dependencies == *"ibmbluemix"* ]]; then
+        # Install RMU utilities
+        installBMCmodule
+      fi
+    fi
+
+    # Install Dev mode utilities
+    installAgent && setupHandlerBinaries && updateStartCommands && generateAppMgmtInfo
+  fi
+}
+
+installAppManagement
---
+++ lib/bluemix_ext.sh
@@ -0,0 +1,127 @@
+# IBM SDK for Node.js Buildpack
+# Copyright 2015 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+install_ibm_node() {
+  local version="$1"
+  local dir="$2"
+
+  # Resolve node version using 'node-version-resolver' within the buildpack if resolution is needed
+  if needs_resolution "$version"; then
+    echo "Resolving node version ${version:-(latest stable)} via 'node-version-resolver'"
+    version=$($BP_DIR/bin/node $BP_DIR/lib/node-version-resolver/index.js "$version")
+  fi
+
+  if [[ ${FIPS_MODE,,} == "true" ]]; then
+    # The FIPS-enabled IBM SDK for Node.js must be in the cache
+    fips_file=$BP_DIR/admin_cache/node/v$version/fips-node-v$version-linux-x64.tar.*
+    verify_fips $fips_file
+    echo "Installing FIPS-enabled IBM SDK for Node.js ($version) from cache"
+    tar xf $fips_file -C /tmp
+  elif [ -f $BP_DIR/admin_cache/node/v$version/node-v$version-linux-x64.tar.* ]; then
+    # Try fetch IBM node runtime from admin cache
+    echo "Installing IBM SDK for Node.js ($version) from cache"
+    tar xf $BP_DIR/admin_cache/node/v$version/node-v$version-linux-x64.tar.* -C /tmp
+  else
+    # Download node from Heroku's S3 mirror of nodejs.org/dist
+    echo "Downloading and installing node $version..."
+    node_url="https://s3pository.heroku.com/node/v$version/node-v$version-linux-x64.tar.gz"
+    curl $node_url --silent --fail --retry 5 --retry-max-time 15 -o - | tar xzf - -C /tmp >/dev/null 2>&1 || (echo "Unable to download node $version; does it exist?" && false)
+  fi
+
+  rm -rf $dir/*
+  mv /tmp/node-v$version-linux-x64/* $dir
+  chmod +x $dir/bin/*
+}
+
+verify_fips () {
+  local fips_file="$1"
+  if [ ! -f $fips_file ]; then
+    FIPS_VERSIONS=""
+    for i in `ls $BP_DIR/admin_cache/node`; do
+      if [ -f $BP_DIR/admin_cache/node/$i/fips-* ]; then
+        FIPS_VERSIONS=$FIPS_VERSIONS" $i"
+      fi
+    done
+    warning "FIPS_MODE is enabled, but the specified node version ($version) is not FIPS-enabled" "Current FIPS-enabled versions: [$FIPS_VERSIONS ]"
+    exit 1
+  fi
+  if [ "$BLUEMIX_APP_MGMT_ENABLE" != "" ]; then
+    warning "Setting FIPS_MODE and BLUEMIX_APP_MGMT_ENABLE at the same time is not supported" "To use FIPS_MODE, unset BLUEMIX_APP_MGMT_ENABLE"
+    exit 1
+  fi
+  if [ "$ENABLE_BLUEMIX_DEV_MODE" != "" ]; then
+    warning "FIPS_MODE and development mode are not supported at the same time" "To use FIPS_MODE, unset ENABLE_BLUEMIX_DEV_MODE"
+    exit 1
+  fi
+}
+
+install_app_management() {
+  #Install App Management
+  if ! [[ ${BLUEMIX_APP_MGMT_INSTALL,,} == "false" ]]; then
+    if ! [[ ${INSTALL_BLUEMIX_APP_MGMT,,} == "false" ]]; then
+      header "Installing App Management"
+      source $BP_DIR/bin/app_management.sh
+    fi
+  fi
+}
+
+npm_install() {
+  module=$1
+  version=$2
+
+  cd $BUILD_DIR
+  # actually using BUILD_DIR/vendor/node/bin/npm, which is set on PATH
+  if [ -z $version ]; then
+    npm install $module --unsafe-perm --quiet --userconfig $BUILD_DIR/.npmrc 2>&1 | output "$LOG_FILE"
+  else
+    npm install $module@"$version" --unsafe-perm --quiet --userconfig $BUILD_DIR/.npmrc 2>&1 | output "$LOG_FILE"
+  fi
+}
+
+add_header_to_bootscript(){
+  header=$1
+  header_for=$2
+  boot_js_file=$($BP_DIR/bin/find_boot_script $BUILD_DIR)
+  if [ "$boot_js_file" != "" ]; then
+    echo "Found start script: $boot_js_file" | output "$LOG_FILE"
+    add_header $BUILD_DIR/$boot_js_file "${header}"
+  else
+    info "WARN: Failed to add 'require' headers to your start script for ${header_for}."
+    info "WARN: Specify your start script in 'package.json' or 'Procfile'."
+  fi
+}
+
+add_header(){
+  file=$1
+  header=$2
+  filename="$(basename $file)"
+  info "Add header to $filename: $header"
+  # check for any shebangs in the start script, insert require headers after the last shebang
+  # if no shebangs exist, it inserts at the top of the file
+  line=$(($(echo $(sed -n '/^#!/ =' $file) | sed -e 's/.*\(.\)$/\1/') + 1))
+  sed -i "${line}i $header" $file
+}
+
+warn_npmrc_registry_override() {
+  local build_dir=${1:-}
+  # if environment var is set && .npmrc exists && .npmrc includes registry, show warning
+  if [ -z "$NPM_CONFIG_REGISTRY" ] || [ -z "$npm_config_registry" ]; then
+    if [ -e "$build_dir/.npmrc" ]; then
+      if grep -iq '^\s*registry\s*\=\s*.*' $build_dir/.npmrc; then
+        info "WARN: 'registry' in the app's '.npmrc' file is overridden by NPM_CONFIG_REGISTRY"
+      fi
+    fi
+  fi
+}
